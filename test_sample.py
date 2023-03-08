ba582712baec6a3df06e8d597998e24777633db7
BhavanaS <127106128+bhavana207@users.noreply.github.com>
2023-03-08 17:33:35 +0530

    Create test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,11 @@
+class TestSample:
+  
+  def test_001(self):
+    print("test 001")
+    
+  def test_002(self):
+    print("test 002")
+    
+  def test_003(self):
+    print("test 003")
+          
